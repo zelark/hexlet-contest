fda466118b5059f62b7d95038a74ae452abbac11
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2017-11-10 08:11:01 +0300

    Update tests (hiccup style)

--- src/test/java/io/hexlet/contest/HtmlBuilderTest.java
+++ src/test/java/io/hexlet/contest/HtmlBuilderTest.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.SneakyThrows;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class HtmlBuilderTest {
@@ -15,18 +16,32 @@ public class HtmlBuilderTest {
 
   @Test
   @SneakyThrows
-  public void test() {
+  public void test_01() {
     List input = mapper.readValue(
-        new File("src/test/resources/test_payload.json"),
+        new File("src/test/resources/test_payload_01.json"),
         ArrayList.class
     );
 
     String result = new HtmlBuilder().build(input);
 
     assertThat(result)
-        .isEqualTo("<html><head><title>hello, hexlet!</title></head><body class=\"container\">"
+        .isEqualTo("<html><head><title>hello, guys!</title></head><body class=\"container\">"
             + "<h1 class=\"header\">html builder example</h1><div>"
-            + "<img class=\"image\" src=\"cat.jpg\"><span>span text</span></div></body></html>"
+            + "<img class=\"image\" src=\"cat.jpg\" /><span>span text <b>lol</b><hr />span text</span>"
+            + "</div></body></html>"
         );
+  }
+
+  @Test
+  @SneakyThrows
+  public void test_02() {
+    List input = mapper.readValue(
+        new File("src/test/resources/test_payload_02.json"),
+        ArrayList.class
+    );
+
+    String result = new HtmlBuilder().build(input);
+
+    assertThat(result).isEqualTo("<span class=\"foo\" id=\"spam\">bar</span>");
   }
 }
